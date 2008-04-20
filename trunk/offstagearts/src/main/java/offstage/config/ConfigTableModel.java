e126827caca34128fd3523f9e90c94625491ed5f
citibob <citibob@633c18f9-3444-0410-88d0-aab395c9f72c>
2008-04-20 02:55:42 +0000

    Temporary Stuff

---
+++ trunk/offstagearts/src/main/java/offstage/config/ConfigTableModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package offstage.config;
+
+import citibob.swing.table.JTypeTableModel;
+import citibob.swing.typed.SwingerMap;
+import java.util.Properties;
+
+/**
+ *
+ * @author citibob
+ */
+public class ConfigTableModel extends BaseJTypeTableModel<Property>
+{
+
+SwingerMap smap;
+
+public ConfigTableModel(Properties props)
+{
+	props.
+}
+	
+	
+}
